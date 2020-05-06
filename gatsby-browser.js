8d52a34d9ec4214709fe3e50fbfd24cfd3d57b4c
yatsubashi <37011432+yatsubashi@users.noreply.github.com>
2020-05-06 16:58:29 +0900

    Feature: Add code title

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,4 @@
 // You can delete this file if you're not using it
 
 require('prismjs/themes/prism.css')
+require('./src/styles/gatsby-remark-prismjs-title.css')
--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
               wrapperStyle: 'margin-bottom: 1rem'
             }
           },
+          'gatsby-remark-prismjs-title',
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
